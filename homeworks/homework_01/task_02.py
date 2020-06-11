fcc130f1e23fbe6f5042e028f08d3867f7ff4043
EgorSilin <EgorSilin@gmail.com>
2020-06-11 20:53:32 +0500

    completed task_02

--- homeworks/homework_01/task_02.py
+++ homeworks/homework_01/task_02.py
@@ -1,1 +1,12 @@
-#text_02
+"""
+Пользователь вводит время в секундах.
+Переведите время в часы, минуты и секунды,
+и выведите в формате чч:мм:сс.
+Используйте форматирование строк.
+"""
+
+seconds = int(input("Введите количество секунд: "))
+hours_full = seconds // 3600
+minutes_full = (seconds % 3600) // 60
+seconds_full = (seconds % 3600) % 60
+print(f"{seconds} секунд равно {hours_full:02}:{minutes_full:02}:{seconds_full:02}.")
