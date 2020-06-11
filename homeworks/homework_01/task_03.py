b3d882d1b72e51ff0665345fde23d2751e2075b6
EgorSilin <EgorSilin@gmail.com>
2020-06-11 21:28:35 +0500

    added line break to task_03

--- homeworks/homework_01/task_03.py
+++ homeworks/homework_01/task_03.py
@@ -8,5 +8,6 @@ number_single = input("Введите число: ")
 number_double = number_single * 2
 number_triple = number_single * 3
 number_sum = int(number_single) + int(number_double) + int(number_triple)
-print(f"Вы ввели число 3 {number_single}, для которого:\nn + nn + nnn = {number_single} + {number_double} + {number_triple} = {number_sum}.")
+print(f"Вы ввели число 3 {number_single}, для которого:\nn + nn + nnn = "
+      f"{number_single} + {number_double} + {number_triple} = {number_sum}.")
 
