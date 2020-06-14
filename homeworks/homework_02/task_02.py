454d78cff7a711999b0a40744136d9601b833f12
EgorSilin <EgorSilin@gmail.com>
2020-06-14 16:46:53 +0500

    completed task_02

--- homeworks/homework_02/task_02.py
+++ homeworks/homework_02/task_02.py
@@ -4,3 +4,11 @@
 При нечетном количестве элементов последний сохранить на своем месте.
 Для заполнения списка элементов необходимо использовать функцию input().
 """
+
+user_list = input("Введите список, разделяя значения запятой:").split(',')
+changed_list = user_list.copy()
+el_number = 0
+for i in range(int(len(changed_list)/2)):
+    changed_list[el_number], changed_list[el_number+1] = changed_list[el_number+1], changed_list[el_number]
+    el_number += 2
+print(f"Вы ввели лист:\n{user_list}\nЛист после перестановки элементов:\n{changed_list}")
