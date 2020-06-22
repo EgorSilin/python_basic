aca7669e0f326e33dc679da2c83ecf7162549b70
EgorSilin <EgorSilin@gmail.com>
2020-06-22 22:51:33 +0500

    completed task_05

--- homeworks/homework_04/task_05.py
+++ homeworks/homework_04/task_05.py
@@ -3,3 +3,15 @@
 Необходимо получить результат вычисления произведения всех элементов списка.
 Подсказка: использовать функцию reduce().
 """
+
+from random import randint
+from functools import reduce
+
+
+def calc_func(prev_el, el):
+    return prev_el * el
+
+
+gen_list = [randint(100, 1000) for el in range(0, 4)]
+print(gen_list)
+print(reduce(calc_func, gen_list))
