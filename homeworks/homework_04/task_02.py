a73d41ba100dba5de8d64c50e74e6469c3d08e4c
EgorSilin <EgorSilin@gmail.com>
2020-06-22 21:38:38 +0500

    completed task_02

--- homeworks/homework_04/task_02.py
+++ homeworks/homework_04/task_02.py
@@ -7,6 +7,5 @@
 """
 
 def_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-# res_list = [el for el in def_list]
 res_list = [def_list[i] for i in range(len(def_list)) if def_list[i] > def_list[i - 1] and i != 0]
 print(res_list)
