02d59dfe302992ebfc8350465a94da39f759edd6
EgorSilin <EgorSilin@gmail.com>
2020-06-24 23:26:32 +0500

    completed task_04

--- homeworks/homework_05/task_04.py
+++ homeworks/homework_05/task_04.py
@@ -7,3 +7,34 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+
+if __name__ == "__main__":
+    # read file to strings list
+    try:
+        with open("text_04.txt", "r", encoding='utf-8') as f_obj:
+            content_str_list = f_obj.readlines()
+    except IOError:
+        print("Произошла ошибка ввода-вывода!")
+
+    # split strings list to word lists
+    content_word_list = [el_str_list.split() for el_str_list in content_str_list]
+
+    # change eng num to rus and join words in strings
+    out_content_str_list = []
+    for el_content_word_list in content_word_list:
+        if el_content_word_list[0] == "One":
+            el_content_word_list[0] = "Один"
+        elif el_content_word_list[0] == "Two":
+            el_content_word_list[0] = "Два"
+        elif el_content_word_list[0] == "Three":
+            el_content_word_list[0] = "Три"
+        elif el_content_word_list[0] == "Four":
+            el_content_word_list[0] = "Четыре"
+        out_content_str_list.append(' '.join(el_content_word_list) + "\n")
+
+    # write new strings in new file
+    try:
+        with open("text_04_new.txt", "w", encoding='utf-8') as f_obj:
+            f_obj.writelines(out_content_str_list)
+    except IOError:
+        print("Произошла ошибка ввода-вывода!")
