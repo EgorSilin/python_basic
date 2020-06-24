c04e8dc8023a7f530066f956a2f2c1b29adb34a5
EgorSilin <EgorSilin@gmail.com>
2020-06-24 21:23:59 +0500

    completed task_01

--- homeworks/homework_05/task_01.py
+++ homeworks/homework_05/task_01.py
@@ -2,3 +2,19 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+
+if __name__ == "__main__":
+
+    user_str_lst = []
+    while True:
+        user_str = input("Введитестроку для записи в файл:")
+        if user_str:
+            user_str_lst.append(user_str + "\n")
+        else:
+            break
+
+    try:
+        with open("text_01.txt", "w", encoding='utf-8') as f_obj:
+            f_obj.writelines(user_str_lst)
+    except IOError:
+        print("Произошла ошибка ввода-вывода!")
