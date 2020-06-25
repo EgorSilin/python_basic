5b2171382b689b2a9a2e615fd261517f999fb2f5
EgorSilin <EgorSilin@gmail.com>
2020-06-25 23:08:05 +0500

    completed task_06

--- homeworks/homework_05/task_06.py
+++ homeworks/homework_05/task_06.py
@@ -13,3 +13,50 @@
 Пример словаря:
 {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
 """
+
+
+def str_to_name(str_par):
+    """take str and return class name"""
+    content_word_list = str_par.split()[0][:-1]
+    return content_word_list
+
+
+def str_to_num_list(str_par):
+    """Taking string with numbers and return list of int numbers
+
+    :param str_par: string
+    :return: list
+    """
+    str_par_len = len(str_par)
+    num_sum = 0
+    i = 0
+    # parse string by symbol cycle
+    while i < str_par_len:
+        num_str = ''
+        current_symbol = str_par[i]
+
+        # check near num symbol cycle
+        while '0' <= current_symbol <= '9':
+            num_str += current_symbol
+            i += 1
+            if i < str_par_len:
+                current_symbol = str_par[i]
+            else:
+                break
+
+        i += 1
+        if num_str != '':
+            num_sum += int(num_str)
+
+    return num_sum
+
+
+if __name__ == "__main__":
+    try:
+        with open("text_06.txt", "r", encoding='utf-8') as f_obj:
+            content_str_list = f_obj.readlines()
+    except IOError:
+        print("Произошла ошибка ввода-вывода!")
+
+    class_dict = {str_to_name(el): str_to_num_list(el) for el in content_str_list}
+    print(class_dict)
