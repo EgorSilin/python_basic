5c8e5b5a8466248b297eb47945199e3c0bcc860c
EgorSilin <EgorSilin@gmail.com>
2020-07-06 21:57:25 +0500

    completed task_02

--- homeworks/homework_08/task_02.py
+++ homeworks/homework_08/task_02.py
@@ -3,3 +3,20 @@
 При вводе пользователем нуля в качестве делителя
 программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+
+class DivByZeroErr(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+if __name__ == '__main__':
+    dividend = float(input("Введите делимое: "))
+    try:
+        divider = float(input("Введите делитель: "))
+        if divider == 0:
+            raise DivByZeroErr("Делить на ноль нельзя!")
+    except DivByZeroErr as er:
+        print(er)
+    else:
+        print(f"Все хорошо. Частное равно: {dividend / divider}")
