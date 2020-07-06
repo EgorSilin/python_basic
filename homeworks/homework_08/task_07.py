f2f2a8708f4e23df2c813d8fcf6780033ee88306
EgorSilin <EgorSilin@gmail.com>
2020-07-07 00:18:10 +0500

    completed task_07

--- homeworks/homework_08/task_07.py
+++ homeworks/homework_08/task_07.py
@@ -4,3 +4,25 @@
 и выполнив сложение и умножение созданных экземпляров.
 Проверьте корректность полученного результата.
 """
+
+
+class ComplexNum:
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
+
+    def __add__(self, other):
+        return complex(self.real + other.real, self.imag + other.imag)
+
+    def __mul__(self, other):
+        return complex(self.real * other.real - self.imag * other.imag, self.real * other.imag + self.imag * other.real)
+
+
+if __name__ == '__main__':
+    print(complex(1, 2) + complex(3, 4))
+    print(ComplexNum(1, 2) + ComplexNum(3, 4))
+    print(complex(1, 2) + complex(3, 4) == ComplexNum(1, 2) + ComplexNum(3, 4))
+
+    print(complex(1, 2) * complex(3, 4))
+    print(ComplexNum(1, 2) * ComplexNum(3, 4))
+    print(complex(1, 2) * complex(3, 4) == ComplexNum(1, 2) * ComplexNum(3, 4))
